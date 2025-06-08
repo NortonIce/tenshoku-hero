a8ed4d4a28e914b9739cce4fc86e0742875816ab
NortonIce <aroares7@gmail.com>
2025-06-08 23:26:08 +0900

    Added display for applied count

--- app/components/listElements/ResumeListElement.tsx
+++ app/components/listElements/ResumeListElement.tsx
@@ -7,13 +7,21 @@ export default function ResumeListElement(props: {
 }) {
   const { resume, onClick, selectedResumeId } = props;
 
+  const usedTimes = (() => {
+    let count = 0;
+    resume?.applications?.forEach((application) => {
+      count += application;
+    });
+    return count;
+  })();
+
   return (
     <div
       key={resume._id}
       className="bg-white rounded-lg shadow p-4 hover:shadow-lg transition-shadow"
     >
       <h3 className=" mb-2">
-        <b>{resume.title}</b>{" "}
+        <b>{resume.title}</b> <div>Applied {usedTimes} times</div>{" "}
         <div className="text-sm ">
           {new Date(resume.createdAt).toLocaleDateString()}
         </div>
